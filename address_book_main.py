1c6aae910603d4637e31f0e42ea24f12515ef677
Himanshu02 <91370829+Himanshu0273@users.noreply.github.com>
2025-04-17 04:32:10 +0530

    Changes to UC2

--- address_book_main.py
+++ address_book_main.py
@@ -1,4 +1,42 @@
-from add_contact_details import AddDetails
+# from add_contact_details import AddDetails
+from contact_info import Contact
+import re
+
+def validate_input(func):
+    def wrapper(*args, **kwargs):
+        fn_pattern=r"^[A-Z][a-z]{2,}$"
+        ln_pattern=r"^[A-Z][a-z]{2,}$"
+        address_pattern=r"^[A-Z][a-zA-Z\s]{1,}$"
+        city_pattern=r"^[A-Z][a-zA-Z\s]{1,}$"
+        state_pattern=r"^[A-Z][a-zA-Z\s]{1,}$"
+        zip_pattern=r"^\d{6}$"
+        phno_pattern=r"^(\+[0-9]{1,3}\s)?[1-9][0-9]{9}$"
+        email_pattern=r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if not re.match(fn_pattern,kwargs['first_name']):
+            raise ValueError("\nInvalid First Name!!")
+        
+        if not re.match(ln_pattern, kwargs['last_name']):
+            raise ValueError("Invalid !!")
+        if not re.match(address_pattern, kwargs['address']):
+            raise ValueError("Invalid Address!!")
+        
+        if not re.match(city_pattern, kwargs['city']):
+            raise ValueError("Invalid City!!")
+        
+        if not re.match(state_pattern, kwargs['state']):
+            raise ValueError("Invalid State!!")
+        
+        if not re.match(zip_pattern, kwargs['zip']):
+            raise ValueError("Invalid ZIP Code!!")
+        
+        if not re.match(phno_pattern, kwargs['phno']):
+            raise ValueError("Invalid Phone Number!!")
+        
+        if not re.match(email_pattern, kwargs['email']):
+            raise ValueError("Invalid Email ID!!")
+        
+        return func(*args, **kwargs)
+    return wrapper
 
 class AddressBookMain:
     
@@ -11,11 +49,25 @@ class AddressBookMain:
     def __iter__(self):
         return iter(self.details)
     
-    def add_contact(self):
-        detail = AddDetails.add_contact()
+    #Add Details
+    @validate_input
+    def add_contact(self,**kwargs):
+        detail =Contact(
+            kwargs["first_name"],
+            kwargs["last_name"],
+            kwargs["address"],
+            kwargs["city"],
+            kwargs["state"],
+            kwargs["zip"],
+            kwargs["phno"],
+            kwargs["email"]
+        )
         self.details.append(detail)
         print("Details added!!!")
         
+    #Edit Details
+    def edit_details(self):
+        pass
     def display_details(self):
         if len(self.details)==0:
             print("Empty Address Book!!!")
--- main.py
+++ main.py
@@ -10,18 +10,40 @@ class AddressBook:
         while True:
             print("""
                 Menu:
-                1. Enter 1 to add contact details
-                9. Enter 9 to display the address book
-                END: Enter 0 to stop
+                1. Enter 1 to add contact details.
+                9. Enter 9 to display the address book.
+                END: Enter 0 to stop.
                 """)
             choice = int(input("Enter your choice: "))
             
             match choice:
                 case 1:
                     print("Give your details to add to the address book: ")
-                    address_book.add_contact()
+                    first_name=input("First Name: ")
+                    last_name=input("Last Name: ")
+                    address=input("Address: ")
+                    city=input("City: ")
+                    state=input("State: ")
+                    zip=input("ZIP Code: ")
+                    phno=input("Phone Number: ")
+                    email=input("Email ID: ")
+                    try:
+                        address_book.add_contact(
+                            first_name=first_name,
+                            last_name=last_name,
+                            address=address,
+                            city=city,
+                            state=state,
+                            zip=zip,
+                            phno=phno,
+                            email=email                            
+                        )              
+                    except ValueError as e:
+                        print(e)
+                    
+                    
                 # case 2:
-                #     print("Chosen number : 2")
+                #     print("Enter the first name to edit details: ")
                 case 9:
                     print("Here's your address book: ")
                     address_book.display_details()
